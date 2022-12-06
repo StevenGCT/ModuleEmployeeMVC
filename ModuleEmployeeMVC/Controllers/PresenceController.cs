7b2004c39fbd604705a8b027ae5f6c5130cd17ab
Steven Gabriel Claros Tapia <cts0028527@est.univalle.edu>
2022-12-06 17:21:47 -0400

    fix2-Migration

---
+++ ModuleEmployeeMVC/Controllers/PresenceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModuleEmployeeMVC.Controllers
+{
+    public class PresenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
